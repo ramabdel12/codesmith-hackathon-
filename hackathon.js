9f46a9469847764d08fd2b4e3fdde491db45ef9b
ramabdel12 <77136023+ramabdel12@users.noreply.github.com>
2022-08-11 02:54:05 +0200

    Tonights hack

---
+++ hackathon.js
@@ -0,0 +1,9 @@
+let notButton = document.querySelector("#not");
+
+notButton.addEventListener( "click", function(){
+    let newDiv = document.createElement('div')
+    douc
+    newDiv.innerText = "Why you wasting my time!"
+},0)
+
+
